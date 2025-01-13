4dfabe82782d38043b264f8d26ceec72e1e6d4cc
soh003 <149380282+soh003@users.noreply.github.com>
2025-01-13 12:36:14 +0100

    Add og get all fungerer

--- js/main.js
+++ js/main.js
@@ -1,22 +1,53 @@
 const app = Vue.createApp({
     data(){
         return{
-            intro:'Indtast kunder til kundedatabase',
-            navn: null,
-            efternavn: null,
-            id: null,
-            kundeListe: [],
+            intro:'Participants',
+            participants: [],
+            newParticipant:{
+            name:'',
+            age: null,
+            country: '',
+            },  
+            
+            
 
         }
     }, 
     methods: {
         addMethod(){
-            this.kundeListe.push({navn:this.navn, efternavn:this.efternavn, id:this.id})
-            this.navn=null;
-            this.efternavn=null;
-            this.id=null;
+            const newParticipant = {
+                name: this.newParticipant.name,
+                age: this.newParticipant.age,
+                country: this.newParticipant.country,
+                
+            };
 
+            axios.post('http://localhost:5071/api/Participants', newParticipant)
+            .then(Response=>{
+                console.log('Participant added', Response.data);
+
+            //Opdaterer tabellen
+            this.getAll();
+            //Ryd inputfelter    
+            this.newParticipant = { name: '', age: '', country: '' };
+
+                
+            })
+            .catch(error=>{
+                console.log(error);
+            })
         },
+        getAll(){
+            axios.get('http://localhost:5071/api/Participants')
+            .then(Response=>{
+                this.participants=Response.data;
+            })
+            .catch(
+                error=>{
+                    console.log(error)
+                }
+              )
+            }
     },
     computed:{
         myComputed(){
